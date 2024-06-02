f6f5e44ccf807120328e2db7f30448b81819870f
Gabriel Sales <ghe_sales@hotmail.com>
2024-06-02 12:00:11 -0300

    Add extension to create to paging

---
+++ Spark.Library/Extensions/Queryable/ResponsePage.cs
@@ -0,0 +1,18 @@
+﻿namespace Spark.Library.Extensions.Queryable
+{
+    public class ResponsePage<T>
+    {
+
+        public ResponsePage()
+        {
+            
+        }
+        public ResponsePage(T? data)
+        {
+            Data = data;
+        }
+
+
+        public T? Data { get; set; }
+    }
+}
